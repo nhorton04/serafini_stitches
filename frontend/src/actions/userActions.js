db234037ceb9902e5f65230cf28991e75f6ce92a
Nick Horton <nhorton04@gmail.com>
2021-01-15 00:00:42 -0800

    login screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,8 @@ import { PRODUCT_LIST_FAIL } from '../constants/productConstants';
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  //   USER_LOGIN_FAIL,
+  //   USER_LOGOUT,
 } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
