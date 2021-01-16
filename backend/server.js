a08a556744587f9268a57a3cc9a3639337bfda24
Nick Horton <nhorton04@gmail.com>
2021-01-15 23:08:23 -0800

    user login

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-  5000,
+  PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.rainbow
       .bold
