e322adc854cca1c0ab3cdca8f542bcc7008cc8ed
Nick Horton <nhorton04@gmail.com>
2021-01-17 23:37:59 -0800

    o rderscreen

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import CheckoutSteps from '../components/CheckoutSteps';
+
+const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address:</strong>
+              </p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default PlaceOrderScreen;
